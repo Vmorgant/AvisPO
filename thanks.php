0b620fba76d8049f60b18be77a437e4bbbb0e0fd
Victor Morgant <victor.morgant@gmail.com>
2020-02-04 17:39:26 +0100

    animation fix

--- thanks.php
+++ thanks.php
@@ -4,19 +4,35 @@
 $imagesDir = 'Image/';
 $images = glob($imagesDir . '*.{jpg,jpeg,png,gif}', GLOB_BRACE);
 $randomImage = $images[array_rand($images)];
-$name=$randomImage;
+$name=basename($randomImage,".png");
 echo ('
-<div class="container h-100">
-    <div class="row h-100 justify-content-center align-items-center">
-        <div class="col-12">
-            <div class="card bg-dark">
-              <img class="card-img-top" src="'.$randomImage.'" alt="Votre Ensipouic">
-              <div class="card-body">
-                <h5 class="card-title text-light">'.$name.' vous remercie</h5>
-                <p class="card-text text-light">Nous espérons que vous avez appréciés ces portes ouvertes et espérons vous revoir l\'année prochaine.</p>
-                <a href="https://sondage.univ-lemans.fr/limesurvey/index.php/599327?lang=fr" class="card-link">Nouveau questionnaire</a>
-                <a href="http://ensim.univ-lemans.fr/fr/index.html" class="card-link">Le site de l\'ENSIM</a>
-              </div>
+<head>
+    <meta charset="UTF-8">
+    <title> Portes Ouvertes </title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<div class="animation-area">
+    <div class="container h-100">
+        <div class="row h-100 justify-content-center align-items-center">
+           <ul class="box-area">
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            <li></li>
+            </ul>
+            <div class="col-12">
+                <div class="card bg-transparent">
+                  <img class="card-img-top" src="'.$randomImage.'" alt="Votre Ensipouic">
+                  <div class="card-body bg-dark">
+                    <h5 class="card-title text-light">'.$name.' vous remercie</h5>
+                    <p class="card-text text-light">Nous espérons que vous avez appréciés ces portes ouvertes et espérons vous revoir l\'année prochaine.</p>
+                    <a href="https://sondage.univ-lemans.fr/limesurvey/index.php/599327?lang=fr" class="card-link">Nouveau questionnaire</a>
+                    <a href="http://ensim.univ-lemans.fr/fr/index.html" class="card-link">Le site de l\'ENSIM</a>
+                  </div>
+                </div>
             </div>
         </div>
     </div>
